1c73e0ab775262638bb3e8933332b2a03d7b2a07
Gustavo Duarte <guga9055@gmail.com>
2023-11-22 15:30:22 -0300

    os dados estão sendo colocados em uma lista ao invés de um dicionário

--- tarefas/index.js
+++ tarefas/index.js
@@ -3,8 +3,18 @@ const express = require("express");
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const tarefas = {};
+const tarefas = [];
 contador = 0;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.get("/get/tarefas", (req, res) => {
   res.send(tarefas);
@@ -14,10 +24,8 @@ app.post("/post/tarefas", (req, res) => {
   contador++;
   console.log(JSON.stringify(req.body));
   const tarefa = req.body;
-  tarefas[contador] = {
-    tarefa,
-  };
-  res.status(201).send(tarefas[contador]);
+  tarefas.push(tarefa);
+  res.status(201).send(tarefa);
 });
 
 app.listen(4000, () => {
