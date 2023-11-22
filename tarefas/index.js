139794b916e40e05798709940777a4c24ac12544
Gustavo Duarte <guga9055@gmail.com>
2023-11-22 14:56:47 -0300

    micro serviço tarefas

--- tarefas/index.js
+++ tarefas/index.js
@@ -1,28 +1,25 @@
-const express = require('express');
-const axios = require ('axios');
+var bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const tarefas = {};
 contador = 0;
 
-app.get('/tarefas', (req, res) => {
-    res.send(tarefas);
+app.get("/get/tarefas", (req, res) => {
+  res.send(tarefas);
 });
 
-
-app.post('/tarefas', async (req, res) => {
-    contador++;
-    const { nome } = req.body;
-    const { data } = req.body;
-    const { categoria } = req.body;
-    const { cor } = req.body;
-    const { descricao } = req.body;
-    tarefas[contador] = {
-        contador, nome, data, categoria, cor, descricao
-    }
-    res.status(201).send(tarefas[contador]);
+app.post("/post/tarefas", (req, res) => {
+  contador++;
+  console.log(JSON.stringify(req.body));
+  const tarefa = req.body;
+  tarefas[contador] = {
+    tarefa,
+  };
+  res.status(201).send(tarefas[contador]);
 });
 
 app.listen(4000, () => {
-    console.log('tarefas. Porta 4000');
+  console.log("tarefas. Porta 4000");
 });
